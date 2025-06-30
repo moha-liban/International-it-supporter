a73660b76f6e4f5511752fa30c9ef5aa57f7ca65
moha-liban <mohacadeofficial@gmail.com>
2025-06-30 15:36:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+#global conistant 
+votingange=18
+  def maim():
+      first_name=input("enter your first name")
+      second_name=input("enter your second name")
+      age=int("enter your age")
+      check_voltting(first_name,second_name)
+      
